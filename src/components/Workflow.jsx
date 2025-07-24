e14830403b79fd6cb84faa0a30db2c73dddfdcfd
Pranay Reddy <55372215+pranayaleti@users.noreply.github.com>
2025-07-24 14:18:00 -0600

    Update Workflow.jsx

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -6,7 +6,7 @@ const Workflow = () => {
   return (
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
-      Build, Launch, Scale.
+      Build, Launch, Scale.<br/>
         <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
         From MVP's to enterprise apps.
         </span>
